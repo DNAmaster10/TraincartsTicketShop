2b1c0b5cb3a215619c159cdd701a32940f69eafb
DNAmaster10 <louishollobon@outlook.com>
2024-02-03 15:23:37 +0000

    Database table changes (Accessors still need changing)

--- src/main/java/com/dnamaster10/tcgui/util/database/TableCreator.java
+++ src/main/java/com/dnamaster10/tcgui/util/database/TableCreator.java
@@ -13,64 +13,99 @@ public class TableCreator extends DatabaseAccessor {
 
     public void createTables() throws SQLException {
         try (Connection connection = getConnection()) {
-            PreparedStatement statement1 = connection.prepareStatement("""
+            PreparedStatement statement;
+            statement = connection.prepareStatement("""
                     CREATE TABLE IF NOT EXISTS players (
-                        id int UNIQUE AUTO_INCREMENT,
+                        id int AUTO_INCREMENT PRIMARY KEY,
                         username varchar(17),
-                        uuid varchar(50) UNIQUE
-                    );
+                        uuid varchar(50) UNIQUE NOT NULL
+                    ) ENGINE=INNODB;
                     """);
-            statement1.execute();
+            statement.execute();
 
-            PreparedStatement statement2 = connection.prepareStatement("""
+            statement = connection.prepareStatement("""
                     CREATE TABLE IF NOT EXISTS guis (
                         id int AUTO_INCREMENT PRIMARY KEY,
+                        owner_id int,
                         name varchar(100) UNIQUE,
                         display_name varchar(100),
                         raw_display_name varchar(100),
-                        owner_uuid varchar(50)
-                    );
+                        FOREIGN KEY (owner_id) REFERENCES players(id)
+                            ON DELETE SET NULL
+                    ) ENGINE=INNODB;
                     """);
-            statement2.execute();
+            statement.execute();
 
-            PreparedStatement statement3 = connection.prepareStatement("""
+            statement = connection.prepareStatement("""
                     CREATE TABLE IF NOT EXISTS tickets (
                         id int AUTO_INCREMENT PRIMARY KEY,
-                        guiid int,
+                        gui_id int,
                         page int,
                         slot int,
                         tc_name varchar(100),
                         display_name varchar(100),
                         raw_display_name varchar(100),
                         price int,
-                        UNIQUE KEY gui_page_slot_unique (guiid, page, slot)
-                    );
+                        UNIQUE KEY gui_page_slot_unique (gui_id, page, slot),
+                        FOREIGN KEY (gui_id) REFERENCES guis(id)
+                            ON DELETE CASCADE
+                    ) ENGINE=INNODB;
                     """);
-            statement3.execute();
+            statement.execute();
 
-            PreparedStatement statement4 = connection.prepareStatement("""
-                    CREATE TABLE IF NOT EXISTS guieditors (
-                        id int UNIQUE AUTO_INCREMENT,
-                        guiid int,
-                        player_uuid varchar(50)
-                    );
-                    """);
-            statement4.execute();
-
-            PreparedStatement statement5 = connection.prepareStatement("""
+            statement = connection.prepareStatement("""
                     CREATE TABLE IF NOT EXISTS linkers (
                         id int AUTO_INCREMENT PRIMARY KEY,
-                        guiid int,
+                        gui_id int,
                         page int,
                         slot int,
-                        linked_guiid int,
+                        linked_gui_id int,
                         linked_gui_page int,
                         display_name varchar(100),
                         raw_display_name varchar(100),
-                        UNIQUE KEY gui_page_slot (guiid, page, slot)
-                    );
+                        UNIQUE KEY gui_page_slot (gui_id, page, slot),
+                        FOREIGN KEY (gui_id) REFERENCES guis(id)
+                            ON DELETE CASCADE
+                    ) ENGINE=INNODB;
                     """);
-            statement5.execute();
+            statement.execute();
+
+            statement = connection.prepareStatement("""
+                    CREATE TABLE IF NOT EXISTS guieditors (
+                        id int AUTO_INCREMENT PRIMARY KEY,
+                        gui_id int NOT NULL,
+                        player_id int NOT NULL,
+                        FOREIGN KEY (player_id) REFERENCES players(id)
+                            ON DELETE CASCADE,
+                        FOREIGN KEY (gui_id) REFERENCES guis(id)
+                            ON DELETE CASCADE
+                    ) ENGINE=INNODB;
+                    """);
+            statement.execute();
+
+            statement = connection.prepareStatement("""
+                    CREATE TABLE IF NOT EXISTS companies (
+                        id int AUTO_INCREMENT PRIMARY KEY,
+                        owner_id int,
+                        company_name varchar(100) UNIQUE,
+                        FOREIGN KEY (owner_id) REFERENCES players(id)
+                            ON DELETE SET NULL
+                    ) ENGINE=INNODB;
+                    """);
+            statement.execute();
+
+            statement = connection.prepareStatement("""
+                    CREATE TABLE IF NOT EXISTS companymembers (
+                        id int AUTO_INCREMENT PRIMARY KEY,
+                        company_id int NOT NULL,
+                        member_id int NOT NULL,
+                        FOREIGN KEY (company_id) REFERENCES companies(id)
+                            ON DELETE CASCADE,
+                        FOREIGN KEY (member_id) REFERENCES players(id)
+                            ON DELETE CASCADE
+                    ) ENGINE=INNODB;
+                    """);
+            statement.execute();
         }
     }
 }
