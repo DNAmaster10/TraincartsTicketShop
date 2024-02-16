1716c928559529335f68ee97be445016d580b057
DNAmaster10 <louishollobon@outlook.com>
2024-02-16 12:46:31 +0000

    Finished Editor List Command

--- src/main/java/com/dnamaster10/traincartsticketshop/commands/commandhandlers/editor/EditorListCommandHandler.java
+++ src/main/java/com/dnamaster10/traincartsticketshop/commands/commandhandlers/editor/EditorListCommandHandler.java
@@ -1,8 +1,13 @@
 package com.dnamaster10.traincartsticketshop.commands.commandhandlers.editor;
 
 import com.dnamaster10.traincartsticketshop.commands.commandhandlers.AsyncCommandHandler;
+import com.dnamaster10.traincartsticketshop.util.Utilities;
 import com.dnamaster10.traincartsticketshop.util.database.GuiAccessor;
+import com.dnamaster10.traincartsticketshop.util.database.GuiEditorsAccessor;
 import com.dnamaster10.traincartsticketshop.util.exceptions.DQLException;
+import net.md_5.bungee.api.chat.*;
+import net.md_5.bungee.api.chat.hover.content.Text;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -12,10 +17,14 @@ import java.util.List;
 import static com.dnamaster10.traincartsticketshop.TraincartsTicketShop.getPlugin;
 
 public class EditorListCommandHandler extends AsyncCommandHandler {
-    //TODO command needs finishing and redoing in places
-    //Example command: /traincartsticketshop gui editor list <gui_name> <optional page number>
-    private GuiAccessor guiAccessor;
+    //Example command: /tshop editor list <gui_name> <optional page number>
+
+    private GuiEditorsAccessor editorsAccessor;
     private Integer guiId;
+    private Integer pageNumber;
+    private Integer totalPages;
+
+    private static final int playersPerPage = 5;
     @Override
     protected boolean checkSync(CommandSender sender, String[] args) {
         //Check permissions
@@ -27,16 +36,31 @@ public class EditorListCommandHandler extends AsyncCommandHandler {
         }
 
         //Check syntax
-        if (args.length > 5) {
-            returnInvalidSubCommandError(sender, args[4]);
+        if (args.length > 4) {
+            returnInvalidSubCommandError(sender, args[5]);
             return false;
         }
-        if (args.length < 4) {
+        if (args.length < 3) {
             returnMissingArgumentsError(sender, "/tshop gui editor list <gui_name> <optional page number>");
             return false;
         }
-        if (!checkGuiNameSyntax(args[3])) {
-            returnGuiNotFoundError(sender, args[3]);
+
+        if (args.length == 4) {
+            if (!Utilities.isInt(args[3])) {
+                returnError(sender, "Page number must be an integer");
+                return false;
+            }
+            pageNumber = Integer.parseInt(args[3]);
+            if (pageNumber < 1) {
+                returnError(sender, "Page number cannot be less than 1");
+                return false;
+            }
+        } else {
+            pageNumber = 1;
+        }
+
+        if (!checkGuiNameSyntax(args[2])) {
+            returnGuiNotFoundError(sender, args[2]);
             return false;
         }
 
@@ -45,12 +69,12 @@ public class EditorListCommandHandler extends AsyncCommandHandler {
 
     @Override
     protected boolean checkAsync(CommandSender sender, String[] args) throws DQLException {
-        guiAccessor = new GuiAccessor();
+        GuiAccessor guiAccessor = new GuiAccessor();
 
         //Get the guiID and check that it exists
-        guiId = guiAccessor.getGuiIdByName(args[3]);
+        guiId = guiAccessor.getGuiIdByName(args[2]);
         if (guiId == null) {
-            returnGuiNotFoundError(sender, args[3]);
+            returnGuiNotFoundError(sender, args[2]);
             return false;
         }
 
@@ -65,14 +89,64 @@ public class EditorListCommandHandler extends AsyncCommandHandler {
                 }
             }
         }
-        //Get players UUIDs
-        List<String> uuids = new ArrayList<>();
 
+        editorsAccessor = new GuiEditorsAccessor();
+
+        //Check there are any editors
+        Integer totalEditors = editorsAccessor.getTotalEditors(guiId);
+        if (totalEditors == 0) {
+            returnError(sender, "There are no editors for gui \"" + args[2] + "\"");
+            return false;
+        }
+
+        totalPages = (int) Math.ceil((double) totalEditors / playersPerPage);
+        //Check if the page is valid
+        if (pageNumber > totalPages) {
+            returnError(sender, "Page " + pageNumber + " does not exist");
+            return false;
+        }
         return true;
     }
 
     @Override
     protected void execute(CommandSender sender, String[] args) throws DQLException {
+        String[] editors = editorsAccessor.getEditorUsernames(guiId, (pageNumber - 1) * playersPerPage, playersPerPage);
 
+        //Build message
+        List<TextComponent> lines = new ArrayList<>();
+        lines.add(new TextComponent(ChatColor.AQUA + "Editors for gui \"" + args[2] + "\""));
+        lines.get(0).setBold(true);
+
+        for (String username : editors) {
+            TextComponent message = new TextComponent(ChatColor.WHITE + " - " + username);
+            lines.add(message);
+        }
+
+        TextComponent pageMessage = new TextComponent(ChatColor.AQUA + "");
+        if (pageNumber > 1) {
+            TextComponent prevPage = new TextComponent(ChatColor.AQUA + " <<< ");
+            prevPage.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/tshop editor list " + args[2] + " " + (pageNumber - 1)));
+            prevPage.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(ChatColor.AQUA + "Prev Page")));
+            pageMessage.addExtra(prevPage);
+        }
+        else {
+            pageMessage.addExtra(ChatColor.AQUA + "     ");
+        }
+
+        pageMessage.addExtra(ChatColor.AQUA + "Page " + pageNumber + "/" + totalPages);
+
+        if (pageNumber < totalPages) {
+            TextComponent nextPage = new TextComponent(ChatColor.AQUA + " >>> ");
+            nextPage.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/tshop editor list " + args[2] + " " + (pageNumber + 1)));
+            nextPage.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(ChatColor.AQUA + "Next Page")));
+            pageMessage.addExtra(nextPage);
+        }
+
+        lines.add(pageMessage);
+
+        //Send the message
+        for (TextComponent component : lines) {
+            sender.spigot().sendMessage(component);
+        }
     }
 }
--- src/main/java/com/dnamaster10/traincartsticketshop/util/database/GuiEditorsAccessor.java
+++ src/main/java/com/dnamaster10/traincartsticketshop/util/database/GuiEditorsAccessor.java
@@ -1,5 +1,6 @@
 package com.dnamaster10.traincartsticketshop.util.database;
 
+import com.dnamaster10.traincartsticketshop.util.database.databaseobjects.PlayerDatabaseObject;
 import com.dnamaster10.traincartsticketshop.util.exceptions.DMLException;
 import com.dnamaster10.traincartsticketshop.util.exceptions.DQLException;
 
@@ -7,6 +8,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GuiEditorsAccessor extends DatabaseAccessor {
     public GuiEditorsAccessor() throws DQLException {
@@ -16,7 +19,7 @@ public class GuiEditorsAccessor extends DatabaseAccessor {
     public boolean checkGuiEditorByUuid(int guiId, String uuid) throws DQLException {
         //Returns true if uuid appears in gui edit list for the given gui id
         try (Connection connection = getConnection()) {
-            PreparedStatement statement = connection.prepareStatement("SELECT COUNT(*) FROM guieditors WHERE gui_id=? AND player_uuid=?");
+            PreparedStatement statement = connection.prepareStatement("SELECT COUNT(*) FROM guieditors WHERE gui_id=? AND editor_uuid=?");
             statement.setInt(1, guiId);
             statement.setString(2, uuid);
             ResultSet result = statement.executeQuery();
@@ -30,15 +33,56 @@ public class GuiEditorsAccessor extends DatabaseAccessor {
         }
     }
 
+    public int getTotalEditors(int guiId) throws DQLException {
+        //Returns total number of editors for a gui
+        try (Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement("SELECT COUNT(*) FROM guieditors WHERE gui_id=?");
+            statement.setInt(1, guiId);
+            ResultSet result = statement.executeQuery();
+            int total = 0;
+            if (result.next()) {
+                total = result.getInt(1);
+            }
+            return total;
+        } catch (SQLException e) {
+            throw new DQLException(e);
+        }
+    }
+
+    public String[] getEditorUsernames(int guiId, int startIndex, int limit) throws DQLException {
+        //Returns a list of username of players who are a registered editor of a gui
+        try (Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement("""
+                    SELECT players.username
+                    FROM guieditors
+                    INNER JOIN players ON guieditors.editor_uuid=players.uuid
+                    WHERE guieditors.gui_id = ?
+                    ORDER BY players.username ASC
+                    LIMIT ?,?                    
+                    """);
+            statement.setInt(1, guiId);
+            statement.setInt(2, startIndex);
+            statement.setInt(3, limit);
+            ResultSet result = statement.executeQuery();
+            List<String> editorList = new ArrayList<>();
+            while (result.next()) {
+                editorList.add(result.getString("players.username"));
+            }
+            return editorList.toArray(String[]::new);
+        } catch (SQLException e) {
+            throw new DQLException(e);
+        }
+    }
+
     public void addGuiEditor(int guiId, String uuid) throws DMLException {
         //Adds a player as an editor for the given gui
         try (Connection connection = getConnection()) {
             PreparedStatement statement = connection.prepareStatement( """
-                    INSERT INTO guieditors (gui_id, player_uuid) VALUES (?,?) 
+                    INSERT INTO guieditors (gui_id, editor_uuid)
                     VALUES (?, ?)
                     ON DUPLICATE KEY UPDATE
-                        gui_id=VALUES(gui_id)
-                        player_uuid=VALUES(player_uuid)
+                        gui_id=VALUES(gui_id),
+                        editor_uuid=VALUES(editor_uuid)
                     """);
             statement.setInt(1, guiId);
             statement.setString(2, uuid);
@@ -51,7 +95,7 @@ public class GuiEditorsAccessor extends DatabaseAccessor {
     public void removeGuiEditor(int guiId, String uuid) throws DMLException {
         //Removes an editor from a gui
         try (Connection connection = getConnection()) {
-            PreparedStatement statement = connection.prepareStatement("DELETE FROM guieditors WHERE gui_id=? AND player_uuid=?");
+            PreparedStatement statement = connection.prepareStatement("DELETE FROM guieditors WHERE gui_id=? AND editor_uuid=?");
             statement.setInt(1, guiId);
             statement.setString(2, uuid);
             statement.executeUpdate();
